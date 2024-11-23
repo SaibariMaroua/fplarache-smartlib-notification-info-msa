a0399cffb5f7ecda5078802dd54598df63c45b4b
maroua saibari <saibari@gmail.com>
2024-11-23 23:50:48 +0100

    Add variables for AWS region, ECR repository, ECS cluster, task family, service name, and subnets in variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+ default = "eu-north-1"
+}
+variable "aws_ecr_repository_name"{
+default = "dev-fplarache-smartlib-notification-repo-msa"
+}
+variable "ecs_cluster_name"{
+default = "dev-fplarache-smartlib-notification-fgcluster-msa"
+}
+variable "ecs_task_family"{
+default = "dev-fplarache-smartlib-notification-td-msa"
+}
+variable "ecs_service_name"{
+default = "dev-fplarache-smartlib-notification-fgservice-msa"
+}
+variable "subnets_ids" {
+type = list(string)
+default = ["subnet-0cf9e8ccfb6fd0f77", "subnet-05fac6b2678b41a37", "subnet-00d967507df4b0bfb"]
+}
