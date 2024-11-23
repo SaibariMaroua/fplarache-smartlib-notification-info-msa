5751172f7a10fc431cc731c48534fbe0a98446ac
maroua saibari <saibari@gmail.com>
2024-11-23 22:56:46 +0100

    Add IAM role configuration for ECS

--- main.tf
+++ main.tf
@@ -48,3 +48,25 @@ resource "aws_ecs_service" "catalog_service" {
     }
     desired_count = 1 
 }
+
+//Rôle IAM pour ECS
+resource "aws_iam_role" "ecs_task_execution" {
+    name = "ecsTaskExecutionfatima-Role"
+
+    assume_role_policy = jsonencode({
+        Version="2012-10-17"
+        Statement = [
+            {
+                Effect = "Allow"
+                Principal = {
+                    Service = "ecs-tasks.amazonaws.com"
+                }
+                Action = "sts:AssumeRole"
+            }
+        ]
+    })
+}
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_attachment" {
+    role = aws_iam_role.ecs_task_execution.name
+    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
