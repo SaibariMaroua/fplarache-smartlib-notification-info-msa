3a7ffb9aaa64315d84c741ca80953c7a885af3fb
maroua saibari <saibari@gmail.com>
2024-11-23 22:45:46 +0100

    Add Terraform configuration for ECS cluster deployment

--- main.tf
+++ main.tf
@@ -7,3 +7,7 @@ resource "aws_ecr_repository" "catalog_repo" {
     name = var.aws_ecr_repository_name
     image_tag_mutability = "MUTABLE"
 }
+// Création d’un cluster ECS
+resource "aws_ecs_cluster" "catalog_cluster" {
+    name = var.ecs_cluster_name
+}
